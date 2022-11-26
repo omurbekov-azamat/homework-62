8aa1a1fdd703406b82463aa86a1e765e99741688
omurbekov-azamat <avgusttin8@gmail.com>
2022-11-26 15:06:58 +0600

    commit-3; created component NavBar.tsx;

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import NavBar from "./components/NavBar/NavBar";
 
 function App() {
   return (
-    <div>
-      hi
-    </div>
+    <>
+      <header>
+        <NavBar/>
+      </header>
+    </>
   );
 }
 
---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import {NavLink} from "react-router-dom";
+
+const NavBar: React.FC = () => {
+  return (
+    <div className='navbar navbar-expand-md navbar-dark bg-danger'>
+      <div className='container-fluid'>
+        <span className='navbar-brand'>Test</span>
+        <div className="collapse navbar-collapse">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <NavLink to='/' className="nav-link">
+                Home
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to='/about-us' className="nav-link">
+                About us
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to='/contacts' className="nav-link">
+                Contacts
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
