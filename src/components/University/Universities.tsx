ee2b47ecf69ab6a9fbdf7f79274480f269da0336
omurbekov-azamat <avgusttin8@gmail.com>
2022-11-27 15:42:08 +0600

    commit-6; added preloader and some css styles;

--- src/components/University/Universities.tsx
+++ src/components/University/Universities.tsx
@@ -1,20 +1,35 @@
 import React from 'react';
 import {GotUniversity} from "../../types";
 import University from "./University";
+import './University.css'
 
 interface Props {
   allUniversities: GotUniversity[]
 }
 
 const Universities: React.FC<Props> = ({allUniversities}) => {
+
+  const empty = () => {
+    if (allUniversities.length === 0) {
+      return (
+        <div className='wrap'>
+          <div className='circle circle-2'>
+            loading...
+          </div>
+        </div>
+      )
+    }
+  }
+
   return (
-    <div className='row m-0 p-3 border border-light overflow-auto bg-white' style={{height: '600px', width: '600px'}}>
+    <div className='row m-0 p-3 border border-dark  overflow-auto bg-white' style={{height: '600px', width: '600px'}}>
       {allUniversities.map(university => (
         <University
           key={Math.random() * 999999}
           university={university}
         />
       ))}
+      {empty()}
     </div>
   );
 };
