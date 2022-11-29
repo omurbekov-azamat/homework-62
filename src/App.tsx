0599be1790ec38ed6f1b7af3a0acc28c1e4554f2
omurbekov-azamat <avgusttin8@gmail.com>
2022-11-29 15:49:30 +0600

    commit-14; refactor Home.tsx;

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import React, {useCallback, useEffect, useState} from 'react';
-import axios from "axios";
+import React from 'react';
 import {Routes, Route} from "react-router-dom";
 import NavBar from "./components/NavBar/NavBar";
 import Home from "./containers/Home/Home";
@@ -7,45 +6,9 @@ import AboutUs from "./containers/AboutUs/AboutUs";
 import Contacts from "./containers/Contacts/Contacts";
 import Footer from "./components/Footer/Footer";
 import Chat from "./containers/Chat/Chat";
-import {GotCountry, GotUniversity} from "./types";
 import BurgerShop from "./containers/BurgerShop/BurgerShop";
 
-const mainUrl = 'http://universities.hipolabs.com/search?country=';
-const infoUrl = 'https://restcountries.com/v2/name/';
-
 function App() {
-  const [universities, setUniversities] = useState<GotUniversity[]>([]);
-
-  const fetchData = useCallback(async (url: string) => {
-
-    const universitiesResponse = await axios.get<GotUniversity[]>(mainUrl + url);
-    const aboutCountryResponse = await axios.get<GotCountry[]>(infoUrl + url);
-
-    if (universitiesResponse.data.length !== 0) {
-      const promises = universitiesResponse.data.map(async set => {
-        return {
-          country: set.country,
-          domains: set.domains,
-          name: set.name,
-          capital: aboutCountryResponse.data[0].capital,
-          flag: aboutCountryResponse.data[0].flag,
-        };
-      });
-      const data = await Promise.all(promises);
-      setUniversities(data);
-    } else {
-      alert('Please try another country!');
-    }
-  }, []);
-
-  useEffect(() => {
-    fetchData('kyrgyzstan').catch(console.error);
-  }, [fetchData]);
-
-  const findUniversitiesFromCountry = (newCountry: string) => {
-    fetchData(newCountry).catch(console.error);
-  };
-
   return (
     <div className='container rounded p-0 shadow-lg'>
       <header>
@@ -54,10 +17,7 @@ function App() {
       <main className='overflow-auto bg-light bg-gradient p-4'>
         <Routes>
           <Route path='/' element={(
-            <Home
-              allUniversities={universities}
-              onSubmit={findUniversitiesFromCountry}
-            />
+            <Home/>
           )}/>
           <Route path='/about-us' element={(
             <AboutUs/>
--- src/containers/Home/Home.tsx
+++ src/containers/Home/Home.tsx
@@ -1,29 +1,73 @@
-import React from 'react';
-import {GotContent, GotUniversity} from "../../types";
+import React, {useCallback, useEffect, useState} from 'react';
+import axios from "axios";
 import Universities from "../../components/University/Universities";
 import MainForm from "../../components/MainForm/MainForm";
 import Carousel from "../../components/Carousel/Carousel";
 import Contents from "../../components/Content/Contents";
+import {GotContent, GotCountry, GotUniversity} from "../../types";
 
-interface Props {
-  allUniversities: GotUniversity[]
-  onSubmit: (country: string) => void;
-}
 
-const Home: React.FC<Props> = ({allUniversities, onSubmit}) => {
+const mainUrl = 'http://universities.hipolabs.com/search?country=';
+const infoUrl = 'https://restcountries.com/v2/name/';
+
+const Home = () => {
+  const [universities, setUniversities] = useState<GotUniversity[]>([]);
+
+  const fetchData = useCallback(async (url: string) => {
+
+    const universitiesResponse = await axios.get<GotUniversity[]>(mainUrl + url);
+    const aboutCountryResponse = await axios.get<GotCountry[]>(infoUrl + url);
+
+    if (universitiesResponse.data.length !== 0) {
+      const promises = universitiesResponse.data.map(async set => {
+        return {
+          country: set.country,
+          domains: set.domains,
+          name: set.name,
+          capital: aboutCountryResponse.data[0].capital,
+          flag: aboutCountryResponse.data[0].flag,
+        };
+      });
+      const data = await Promise.all(promises);
+      setUniversities(data);
+    } else {
+      alert('Please try another country!');
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchData('kyrgyzstan').catch(console.error);
+  }, [fetchData]);
+
+  const findUniversitiesFromCountry = (newCountry: string) => {
+    fetchData(newCountry).catch(console.error);
+  };
+
   const someContent: GotContent[] = [
-    {textCss: 'fs-5 order-2 fst-italic', imageCss: 'order-1 me-3', imageUrl: 'https://www.onatlas.com/wp-content/uploads/2019/03/education-students-people-knowledge-concept-P6MBQ5W-1080x675.jpg'},
-    {textCss: 'fs-5 order-1 fw-bolder', imageCss: 'order-2 ms-3', imageUrl: 'https://oregonstate.edu/sites/default/files/paragraphs/linc-quad_0.jpg'},
-    {textCss: 'fs-5 order-2 fw-lighter', imageCss: 'order-1 me-3', imageUrl: 'https://www.bsdc.ac.uk/images/UniversityLevel/Staffordshire-Uni-In-Partnership-Banner-02web.jpg'},
+    {
+      textCss: 'fs-5 order-2 fst-italic',
+      imageCss: 'order-1 me-3',
+      imageUrl: 'https://www.onatlas.com/wp-content/uploads/2019/03/education-students-people-knowledge-concept-P6MBQ5W-1080x675.jpg'
+    },
+    {
+      textCss: 'fs-5 order-1 fw-bolder',
+      imageCss: 'order-2 ms-3',
+      imageUrl: 'https://oregonstate.edu/sites/default/files/paragraphs/linc-quad_0.jpg'
+    },
+    {
+      textCss: 'fs-5 order-2 fw-lighter',
+      imageCss: 'order-1 me-3',
+      imageUrl: 'https://www.bsdc.ac.uk/images/UniversityLevel/Staffordshire-Uni-In-Partnership-Banner-02web.jpg'
+    },
   ];
 
 
   return (
     <div>
       <div className='d-flex justify-content-between pb-5 mb-4 border-bottom border-danger'>
-        <Universities allUniversities={allUniversities}/>
+        <Universities allUniversities={universities}/>
         <div style={{width: '600px'}}>
-          <MainForm onSubmit={onSubmit}/>
+          <MainForm onSubmit={findUniversitiesFromCountry}/>
           <p className='mt-5 fs-4'>
             Мы собрали для Вас все университеты мира в одном месте. Данный сайт поможет людям со всего мира найти
             университет по своим запросам.
